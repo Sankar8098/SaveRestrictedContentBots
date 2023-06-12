8b954baf21ff383a54b0772664612c83b0942c2a
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2023-06-12 06:56:48 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = 23275126
-API_HASH = "532c54bbe1c3f2296981f02469455915"
+API_ID = 23788442
+API_HASH = "72dd69766639a884b8985937d7fb3e43"
 BOT_TOKEN = "6178286330:AAGyOx8tGlxbtD-_PCElEJNajtmmmZz_ehA"
-SESSION = "-rIQxAA-rIQxA"
+SESSION = "BQFq-5oAxJ-sGxPMtuNV8q9dyGVNFjrl4fLdp9aPozjGlKrs2c6zETGCfbO2PdnBcF1vEaWQem_Part7tt2ps9UIPI8OvCnmNEaOBvgHDCse9x3vqe8obSmZzRLfNdUj4WSS43vvsB-DwIxnlakURXQ4S4NWHwUqLo5o9ZFN3uXwO5fLAcT5WpLXSnFvSqK8us5GyN4GVIju1xZ9JmsF_3Yb-X72WpouWi471cft7-nm8uhXs78EvqkViM6wfsvKWapMqLMKnOHJeyKzQ2V_0E41B2l6OunQ6Rh5rOb8SlV_xzjrTI5pPvFWf6nMDTGkAgCaDI94HcmBfcIk9QLe7cXqvBQcTwAAAABNCkZ9AA"
 FORCESUB = "SK_MoviesOffl"
-AUTH = 5677824892
+AUTH = 1292519037
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
