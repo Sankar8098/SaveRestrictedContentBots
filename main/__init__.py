14d6dff5f985d18edc3ad447dc1f516535fbeddc
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2023-08-10 06:06:14 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = 23788442
-API_HASH = "72dd69766639a884b8985937d7fb3e43"
-BOT_TOKEN = "6178286330:AAGyOx8tGlxbtD-_PCElEJNajtmmmZz_ehA"
-SESSION = "BQFq-5oAxJ-sGxPMtuNV8q9dyGVNFjrl4fLdp9aPozjGlKrs2c6zETGCfbO2PdnBcF1vEaWQem_Part7tt2ps9UIPI8OvCnmNEaOBvgHDCse9x3vqe8obSmZzRLfNdUj4WSS43vvsB-DwIxnlakURXQ4S4NWHwUqLo5o9ZFN3uXwO5fLAcT5WpLXSnFvSqK8us5GyN4GVIju1xZ9JmsF_3Yb-X72WpouWi471cft7-nm8uhXs78EvqkViM6wfsvKWapMqLMKnOHJeyKzQ2V_0E41B2l6OunQ6Rh5rOb8SlV_xzjrTI5pPvFWf6nMDTGkAgCaDI94HcmBfcIk9QLe7cXqvBQcTwAAAABNCkZ9AA"
+API_ID = 24329924
+API_HASH = "395fe20d8f4439e2f5bb1371533a25d3"
+BOT_TOKEN = "6107891601:AAGXMd98Uoye5zNtsPmf4OWUqT6XXvSM0rY"
+SESSION = "AQFzPsQAscat2Geg067hEBZdAMrrrB8_j-1EWIqCrd_VZK7Pl3bFWtejlQWuQsH-3ob0YO_sK3EzCdmrmEw_Wvnw27nkCNBaZZclG6VxltgvgybWjWJfSNaFd1eqfl94tZkXcCc4EzEc8C_5IVF9GwF0ZwftI39bIuJQcakYx166HvJxm2k2-DGtPCUoGMr-XC4Aot_QuZDywlniMApUUiLVpwetxW03NrFcVWLbRGHP7bk5kXPZchOm2ydfqLqSWJt0JajpCCmKNo0pvfnYdu5Qb6i7_B_f_4JIavILaYWlTBWujBvVc2BPABmcC6NYL_F9HpxF1uiajtoiUCbeI7YEP-_-QgAAAAFm4TwjAA"
 FORCESUB = "SK_MoviesOffl"
-AUTH = 1292519037
+AUTH = 6021004323
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
